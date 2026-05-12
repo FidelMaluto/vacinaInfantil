085275135df880367f61e566c71bfe397fea7637
Fidel Maluto <fidel.maluto77@.com>
2026-05-12 20:20:16 +0100

    feat: Novas funcionalidade e inclemento do npm

---
+++ backend/servidor.js
@@ -0,0 +1,212 @@
+// VACINA-INFANTIL - SERVER NODE.JS
+
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+// CONFIGURAÇÕES
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// CONEXÃO COM MYSQL
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Angola@123',
+    database: 'vacinainfantil'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log('Erro ao conectar no banco:', err);
+        return;
+    }
+    console.log('✅ Banco de dados conectado!');
+});
+
+// CONFIGURAÇÃO DO EMAIL
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'SEUEMAIL@gmail.com',
+        pass: 'SUA_SENHA_DE_APP'
+    }
+});
+
+// ROTA TESTE
+
+app.get('/', (req, res) => {
+    res.send('🚀 API Vacina-Infantil funcionando!');
+});
+
+// CADASTRAR USUÁRIO
+
+app.post('/usuarios', (req, res) => {
+    const { nome, email, telefone, senha, tipo } = req.body;
+
+    const sql = `INSERT INTO usuarios(nome, email, telefone, senha, tipo) VALUES (?, ?, ?, ?, ?)`;
+
+    db.query(sql, [nome, email, telefone, senha, tipo],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    erro: 'Erro ao cadastrar usuário'
+                });
+            }
+
+            res.json({ mensagem: '✅ Usuário cadastrado com sucesso!' });
+        });
+});
+
+// CADASTRAR CRIANÇA
+
+app.post('/criancas', (req, res) => {
+    const { usuario_id, nome, nascimento, sexo } = req.body;
+
+    const sql = `INSERT INTO criancas(usuario_id, nome, nascimento, sexo) VALUES (?, ?, ?, ?)`;
+
+    db.query(sql, [usuario_id, nome, nascimento, sexo],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    erro: 'Erro ao cadastrar criança'
+                });
+            }
+
+            res.json({ mensagem: '👶 Criança cadastrada com sucesso!' });
+
+        });
+});
+
+// LISTAR VACINAS
+
+app.get('/vacinas', (req, res) => {
+    const sql = `SELECT * FROM vacinas`;
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({
+                erro: 'Erro ao buscar vacinas'
+            });
+        }
+        res.json(results);
+    });
+});
+
+// REGISTRAR VACINAÇÃO
+
+app.post('/vacinacoes', (req, res) => {
+    const { crianca_id, vacina_id, data_aplicacao, proxima_dose } = req.body;
+
+    const sql = `INSERT INTO vacinacoes(crianca_id, vacina_id, data_aplicacao, proxima_dose,status) VALUES (?, ?, ?, ?, ?)`;
+
+    db.query(sql,[crianca_id,vacina_id,data_aplicacao,proxima_dose,'Tomada'],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.statu(500).json({
+                    erro: 'Erro ao registrar vacinação'
+                });
+            }
+
+            res.json({mensagem: '💉 Vacinação registrada!'});
+
+        });
+});
+
+// ENVIAR NOTIFICAÇÃO POR EMAIL
+
+app.post('/enviar-email', (req, res) => {
+    const {email,nomeCrianca,vacina,data} = req.body;
+
+    const mailOptions = {
+        from: 'SEUEMAIL@gmail.com',
+        to: email,
+        subject: '💉 Lembrete de Vacinação',
+        html: `        
+            <div style="font-family:Arial;padding:20px;">
+
+                <h2 style="color:#2563eb;">
+                    VacinaKids
+                </h2>
+
+                <p>
+                    Olá,
+                </p>
+
+                <p>
+                    A criança <strong>${nomeCrianca}</strong>
+                    deve tomar a vacina
+                    <strong>${vacina}</strong>.
+                </p>
+
+                <p>
+                    Data prevista:
+                    <strong>${data}</strong>
+                </p>
+
+                <br>
+
+                <p>
+                    Não esqueça da vacinação 💙
+                </p>
+
+            </div>
+        `
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({
+                erro: 'Erro ao enviar email'
+            });
+
+        }
+
+        res.json({mensagem: '📧 Email enviado com sucesso!'});
+    });
+});
+
+// LISTAR CRIANÇAS
+
+app.get('/criancas', (req, res) => {
+
+    const sql = `SELECT criancas.*, usuarios.nome AS responsavel FROM criancas JOIN usuarios
+        ON criancas.usuario_id = usuarios.id `;
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({
+                erro: 'Erro ao buscar crianças'
+            });
+
+        }
+
+        res.json(results);
+
+    });
+});
+
+// SERVIDOR
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em: http://localhost:${PORT}`);
+});
--- public/app.js
+++ public/app.js
@@ -1,19 +1,206 @@
-function entrar(){
+// URL DA API
 
-  const email = document.getElementById('email').value;
-  const senha = document.getElementById('senha').value;
+const API = 'http://localhost:3000';
 
-  if(email === '' || senha === ''){
+// CADASTRAR USUÁRIO
 
-    alert('Preencha todos os campos!');
+async function cadastrarUsuario() {
+  const dados = {
+    nome: document.getElementById('nome').value,
+    email: document.getElementById('email').value,
+    telefone: document.getElementById('telefone').value,
+    senha: document.getElementById('senha').value,
+    tipo: 'pai'
 
-    return;
   };
 
-  document.getElementById('loginPage').style.display = 'none';
+  try {
+    const response = await fetch(`${API}/usuarios`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(dados)
+    });
 
-  document.getElementById('sistema').classList.remove('hidden');
+    const resultado = await response.json();
 
-  alert('Login realizado com sucesso!');
+    alert(resultado.mensagem);
 
-};
+  } catch (erro) {
+    console.log(erro);
+    alert('Erro ao cadastrar usuário');
+  }
+}
+
+// CADASTRAR CRIANÇA
+
+async function cadastrarCrianca() {
+  const dados = {
+    usuario_id: document.getElementById('usuario_id').value,
+    nome: document.getElementById('nomeCrianca').value,
+    nascimento: document.getElementById('nascimento').value,
+    sexo: document.getElementById('sexo').value
+  };
+
+  try {
+    const response = await fetch(`${API}/criancas`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(dados)
+    });
+
+    const resultado = await response.json();
+
+    alert(resultado.mensagem);
+
+  } catch (erro) {
+    console.log(erro);
+    alert('Erro ao cadastrar criança');
+  }
+}
+
+// LISTAR VACINAS
+
+async function listarVacinas() {
+  try {
+    const response = await fetch(`${API}/vacinas`);
+    const vacinas = await response.json();
+    const container = document.getElementById('listaVacinas');
+
+    container.innerHTML = '';
+    vacinas.forEach(vacina => {
+      container.innerHTML += `
+            
+                <div class="vacina-card">
+
+                    <img src="${vacina.imagem}" width="100%">
+
+                    <div class="vacina-info">
+
+                        <h3>${vacina.nome}</h3>
+
+                        <p>
+                            ${vacina.descricao}
+                        </p>
+
+                        <p>
+                            <strong>Idade:</strong>
+                            ${vacina.idade_recomendada}
+                        </p>
+
+                        <p>
+                            <strong>Cuidados:</strong>
+                            ${vacina.cuidados}
+                        </p>
+
+                    </div>
+
+                </div> `;
+    });
+
+  } catch (erro) {
+    console.log(erro);
+  }
+}
+
+// REGISTRAR VACINAÇÃO
+
+async function registrarVacinacao() {
+  const dados = {
+    crianca_id: document.getElementById('crianca_id').value,
+    vacina_id: document.getElementById('vacina_id').value,
+    data_aplicacao: document.getElementById('data_aplicacao').value,
+    proxima_dose: document.getElementById('proxima_dose').value
+  };
+
+  try {
+    const response = await fetch(`${API}/vacinacoes`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(dados)
+
+    });
+
+    const resultado = await response.json();
+
+    alert(resultado.mensagem);
+
+  } catch (erro) {
+    console.log(erro);
+    alert('Erro ao registrar vacinação');
+  }
+}
+
+// ENVIAR EMAIL
+
+async function enviarEmail() {
+
+  const dados = {
+    email: document.getElementById('emailResponsavel').value,
+    nomeCrianca: document.getElementById('nomeCriancaEmail').value,
+    vacina: document.getElementById('vacinaEmail').value,
+    data: document.getElementById('dataVacina').value
+  };
+
+  try {
+    const response = await fetch(`${API}/enviar-email`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(dados)
+
+    });
+
+    const resultado = await response.json();
+
+    alert(resultado.mensagem);
+
+  } catch (erro) {
+    console.log(erro);
+    alert('Erro ao enviar email');
+  }
+}
+
+// LISTAR CRIANÇAS
+
+async function listarCriancas() {
+  try {
+    const response = await fetch(`${API}/criancas`);
+    const criancas = await response.json();
+    const container = document.getElementById('listaCriancas');
+
+    container.innerHTML = '';
+    criancas.forEach(crianca => {
+      container.innerHTML += `
+                <div class="card">
+
+                    <h3>
+                        👶 ${crianca.nome}
+                    </h3>
+
+                    <p>
+                        Responsável:
+                        ${crianca.responsavel}
+                    </p>
+
+                    <p>
+                        Sexo:
+                        ${crianca.sexo}
+                    </p>
+
+                    <p>
+                        Nascimento:
+                        ${crianca.nascimento}
+                    </p>
+
+                </div> `;
+    });
+
+  } catch (erro) {
+    console.log(erro);
+  }
+}
+
+// CARREGAR AUTOMATICAMENTE
+
+listarVacinas();
+
+listarCriancas();
